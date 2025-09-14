a8fad4d3120fa80961280d8dd16a4e24072537e6
javierdelamorena <javierdelamorenabriones@hotmail.com>
2025-09-14 14:39:46 +0200

    nd

--- auth-service/src/main/java/com/tutorial/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/tutorial/authservice/AuthServiceApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 
+
+
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
